5935c8c2514c5c7c5fb9d5569b93a240f1f88dc4
CrislainyMartins <143403752+CrislainyMartins@users.noreply.github.com>
2023-10-17 11:19:46 -0300

    a9

--- lista01.sergio.continuacao/16.py
+++ lista01.sergio.continuacao/16.py
@@ -1,6 +1,4 @@
 metros=float(input('digite um tamanho em metros da área a ser pintada '))
-tres=1
-dezoito=80
 latas=metros/(3*18)
 valor=latas*80
 print('a quantidade de tintas é {} e o preço total é {}'.format(latas,valor))

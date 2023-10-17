93e6d56f317adb251aa57d38dfc413f3dcf83ebd
CrislainyMartins <143403752+CrislainyMartins@users.noreply.github.com>
2023-10-17 11:04:34 -0300

    a6

--- lista01.sergio.continuacao/17.py
+++ lista01.sergio.continuacao/17.py
@@ -5,7 +5,6 @@ galoes=3.6
 valor_g=metros*25
 rendimento_latas=(6*18)*metros
 rendimento_galoes=(6*3.6)*metros
-
 print('compra apenas em latas de 18 litros: R$ {}'.format(rendimento_latas))
 print('compra apenas em galoes de 3.6 litros: R$ {}'.format(rendimento_galoes))
 

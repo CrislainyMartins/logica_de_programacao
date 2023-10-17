ed86710efd0b202d4ebe664676818173c82bde0c
CrislainyMartins <143403752+CrislainyMartins@users.noreply.github.com>
2023-10-17 11:03:09 -0300

    a3

---
+++ lista01.sergio.continuacao/16.py
@@ -0,0 +1,4 @@
+metros=float(input('digite um tamanho em metros da área a ser pintada '))
+tres=1
+dezoito=80
+
---
+++ lista01.sergio.continuacao/17.py
@@ -0,0 +1,13 @@
+metros=float(input('digite a area a ser pintada '))
+latas=18
+valor_l=metros*80
+galoes=3.6
+valor_g=metros*25
+rendimento_latas=(6*18)*metros
+rendimento_galoes=(6*3.6)*metros
+
+print('compra apenas em latas de 18 litros: R$ {}'.format(rendimento_latas))
+print('compra apenas em galoes de 3.6 litros: R$ {}'.format(rendimento_galoes))
+
+
+
